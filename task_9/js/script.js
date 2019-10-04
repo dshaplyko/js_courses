dd6fe765b2b795443466574a3e9e4ef78cb15d06
Dzmitry Shaplyka <Dzmitry_Shaplyka@epam.com>
2019-10-04 15:11:24 +0300

    task 9 completed

--- task_9/js/script.js
+++ task_9/js/script.js
@@ -36,7 +36,7 @@ window.addEventListener('DOMContentLoaded', () => {
 
   // Timer
 
-  let deadline = '2019-10-21';
+  let deadline = '2019-10-5';
 
   function getTimeRemaining(endTime) {
     let t = Date.parse(endTime) - Date.parse(new Date()),
@@ -61,12 +61,24 @@ window.addEventListener('DOMContentLoaded', () => {
     
     function updateClock() {
       let t = getTimeRemaining(endTime);
-      hours.textContent = t.hours;
-      minutes.textContent = t.minutes;
-      seconds.textContent = t.seconds;
 
-      if (t.total < 0) {
+      function addZero(num) {
+        if (num <= 9 ) {
+          return '0' + num;
+        } else {
+          return num;
+        }
+      }
+
+      hours.textContent = addZero(t.hours);
+      minutes.textContent = addZero(t.minutes);
+      seconds.textContent = addZero(t.seconds);
+
+      if (t.total <= 0) {
         clearInterval(timeInterval);
+        hours.textContent = '00';
+        minutes.textContent = '00';
+        seconds.textContent = '00';
       }
     }
 
