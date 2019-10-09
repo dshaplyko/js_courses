e4b84ff7e7c95ff9e8d37792fb9a521f27bb24f1
Dzmitry Shaplyka <Dzmitry_Shaplyka@epam.com>
2019-10-09 16:39:16 +0300

    task 14 added

---
+++ task_14/js/script.js
@@ -0,0 +1,37 @@
+
+let inputRub = document.getElementById('rub'),
+    inputUsd = document.getElementById('usd');
+
+inputRub.addEventListener('input', () => {
+
+    function getData() {
+        return new Promise((resolve, reject) => {
+            let request = new XMLHttpRequest();
+
+            request.open('GET', 'js/current.json');
+            request.setRequestHeader('Content-type', 'application/json; charset=utf-8');
+            request.send();
+
+
+            request.onload = function() {
+                if(request.readyState === 4) {
+                    if(request.status == 200) {
+                        resolve(this.response);
+                    }
+                    else {
+                        reject();
+                    
+                    }
+                }
+            };            
+        });
+    }
+
+    getData()
+        .then(response => {
+            let data = JSON.parse(response);
+            inputUsd.value = inputRub.value / data.usd;
+        })
+        .catch(() => inputUsd.value = "Что-то пошло не так");
+    
+});
